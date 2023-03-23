5192e380e39ca48584ef1b407452b15e1d2cbe28
Jakub Nowak <nowakkuba99@gmail.com>
2023-03-23 17:10:49 +0100

    Initial commit - added vendor and CMakeLists.txt

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+// Standard includes
+#include <iostream>
+#include <cmath>
+
+//OpenGL related includes
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+int main()
+{
+    return 0;
+}
